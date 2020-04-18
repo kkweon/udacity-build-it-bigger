14d35fb68d6ee26038680d25d339694293e2b03f
Mo Kweon <kkweon@google.com>
2020-04-17 21:13:08 -0700

    Update JokeFactory tests.

--- joke/src/main/java/dev/kkweon/joke/JokeFactory.java
+++ joke/src/main/java/dev/kkweon/joke/JokeFactory.java
@@ -1,14 +1,13 @@
 package dev.kkweon.joke;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
+import java.util.stream.Collectors;
 
 public class JokeFactory {
 
@@ -18,39 +17,18 @@ public class JokeFactory {
     public static FutureTask<String> getJoke() {
 
         return new FutureTask<>(
-                new Callable<String>() {
-                    @Override
-                    public String call() {
-                        if (jokes == null) {
-                            try {
-                                jokes = readJokesFromTextFile();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                                return null;
-                            }
-                        }
-                        int idx = random.nextInt(jokes.size());
-                        return jokes.get(idx);
+                () -> {
+                    if (jokes == null) {
+                        jokes = readJokesFromTextFile();
                     }
+                    int idx = random.nextInt(jokes.size());
+                    return jokes.get(idx);
                 });
     }
 
-    public static List<String> readJokesFromTextFile() throws IOException {
-        File file = new File(JokeFactory.class.getClassLoader().getResource("jokes.txt").getFile());
-        BufferedReader bufferedReader = null;
-        bufferedReader = new BufferedReader(new FileReader(file));
-
-        List<String> jokes = new ArrayList<>();
-
-        String line;
-
-        while ((line = bufferedReader.readLine()) != null) {
-            line = line.trim();
-            if (!line.isEmpty()) {
-                jokes.add(line);
-            }
-        }
-
-        return jokes;
+    public static List<String> readJokesFromTextFile() throws URISyntaxException, IOException {
+        List<String> jokes =
+                Files.readAllLines(Paths.get(JokeFactory.class.getResource("/jokes.txt").toURI()));
+        return jokes.stream().filter(j -> !j.isEmpty()).collect(Collectors.toList());
     }
 }
--- joke/src/test/java/dev/kkweon/joke/JokeFactoryTests.java
+++ joke/src/test/java/dev/kkweon/joke/JokeFactoryTests.java
@@ -3,8 +3,11 @@ package dev.kkweon.joke;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,11 +15,13 @@ public class JokeFactoryTests {
 
     @Test
     public void testGetJoke() throws ExecutionException, InterruptedException {
-        Assertions.assertNotNull(JokeFactory.getJoke().get());
+        FutureTask<String> joke = JokeFactory.getJoke();
+        Executors.newCachedThreadPool().submit(joke);
+        Assertions.assertNotNull(joke.get());
     }
 
     @Test
-    public void testReadJokesFromTextFile() throws IOException {
+    public void testReadJokesFromTextFile() throws URISyntaxException, IOException {
         List<String> jokes = JokeFactory.readJokesFromTextFile();
         assertEquals(
                 "\"Automatic\" simply means that you cannot repair it yourself.", jokes.get(0));
