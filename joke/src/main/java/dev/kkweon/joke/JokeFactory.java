be866d7af6926b7e99f04843306bc1b121fbdd2b
Mo Kweon <kkweon@google.com>
2020-04-17 20:53:17 -0700

    Add a real joke

--- joke/src/main/java/dev/kkweon/joke/JokeFactory.java
+++ joke/src/main/java/dev/kkweon/joke/JokeFactory.java
@@ -1,16 +1,56 @@
 package dev.kkweon.joke;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 
 public class JokeFactory {
+
+    private static List<String> jokes = null;
+    private static Random random = new Random();
+
     public static FutureTask<String> getJoke() {
+
         return new FutureTask<>(
                 new Callable<String>() {
                     @Override
                     public String call() {
-                        return "Knock Knock";
+                        if (jokes == null) {
+                            try {
+                                jokes = readJokesFromTextFile();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                return null;
+                            }
+                        }
+                        int idx = random.nextInt(jokes.size());
+                        return jokes.get(idx);
                     }
                 });
+    }
+
+    public static List<String> readJokesFromTextFile() throws IOException {
+        File file = new File(JokeFactory.class.getClassLoader().getResource("jokes.txt").getFile());
+        BufferedReader bufferedReader = null;
+        bufferedReader = new BufferedReader(new FileReader(file));
+
+        List<String> jokes = new ArrayList<>();
+
+        String line;
+
+        while ((line = bufferedReader.readLine()) != null) {
+            line = line.trim();
+            if (!line.isEmpty()) {
+                jokes.add(line);
+            }
+        }
+
+        return jokes;
     }
 }
--- joke/src/test/java/dev/kkweon/joke/JokeFactoryTests.java
+++ joke/src/test/java/dev/kkweon/joke/JokeFactoryTests.java
@@ -2,13 +2,23 @@ package dev.kkweon.joke;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class JokeFactoryTests {
 
     @Test
     public void testGetJoke() throws ExecutionException, InterruptedException {
-        assertEquals("Knock Knock", JokeFactory.getJoke().get());
+        Assertions.assertNotNull(JokeFactory.getJoke().get());
+    }
+
+    @Test
+    public void testReadJokesFromTextFile() throws IOException {
+        List<String> jokes = JokeFactory.readJokesFromTextFile();
+        assertEquals(
+                "\"Automatic\" simply means that you cannot repair it yourself.", jokes.get(0));
     }
 }
